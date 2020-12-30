5c3fdd47935f1906ff100b735e46b1d4666df7d7
Senthilkumar Mathivanan <msenyoo@gmail.com>
2020-12-30 07:32:56 +0530

    npm package name updated

--- scripts/copyBuildFiles.js
+++ scripts/copyBuildFiles.js
@@ -1,38 +1,27 @@
-var path = require('path');
-var fse = require('fs-extra');
+var path = require("path");
+var fse = require("fs-extra");
 
-const files = [
-  'README.md',
-  'LICENSE'
-];
+const files = ["README.md", "LICENSE"];
 
-Promise.all(
-  files.map((file) => copyFile(file))
-)
-.then(() => createPackageFile());
+Promise.all(files.map(file => copyFile(file))).then(() => createPackageFile());
 
 function copyFile(file) {
   const libPath = resolveBuildPath(file);
-  return new Promise((resolve) => {
-    fse.copy(
-      file,
-      libPath,
-      (err) => {
-        if (err) throw err;
-        resolve();
-      }
-    );
-  })
-  .then(() => console.log(`Copied ${file} to ${libPath}`));
+  return new Promise(resolve => {
+    fse.copy(file, libPath, err => {
+      if (err) throw err;
+      resolve();
+    });
+  }).then(() => console.log(`Copied ${file} to ${libPath}`));
 }
 
 function resolveBuildPath(file) {
-  return path.resolve(__dirname, '../lib/', path.basename(file));
+  return path.resolve(__dirname, "../lib/", path.basename(file));
 }
 
 function createPackageFile() {
-  return new Promise((resolve) => {
-    fse.readFile(path.resolve(__dirname, '../package.json'), 'utf8', (err, data) => {
+  return new Promise(resolve => {
+    fse.readFile(path.resolve(__dirname, "../package.json"), "utf8", (err, data) => {
       if (err) {
         throw err;
       }
@@ -40,44 +29,44 @@ function createPackageFile() {
       resolve(data);
     });
   })
-  .then((data) => JSON.parse(data))
-  .then((packageData) => {
-    const {
-      author,
-      version,
-      description,
-      keywords,
-      repository,
-      license,
-      bugs,
-      homepage,
-      peerDependencies,
-      dependencies,
-    } = packageData;
+    .then(data => JSON.parse(data))
+    .then(packageData => {
+      const {
+        author,
+        version,
+        description,
+        keywords,
+        repository,
+        license,
+        bugs,
+        homepage,
+        peerDependencies,
+        dependencies
+      } = packageData;
 
-    const minimalPackage = {
-      name: 'ps-react',
-      author,
-      version,
-      description,
-      main: './index.js',
-      keywords,
-      repository,
-      license,
-      bugs,
-      homepage,
-      peerDependencies,
-      dependencies
-    };
+      const minimalPackage = {
+        name: "react-shared-components",
+        author,
+        version,
+        description,
+        main: "./index.js",
+        keywords,
+        repository,
+        license,
+        bugs,
+        homepage,
+        peerDependencies,
+        dependencies
+      };
 
-    return new Promise((resolve) => {
-      const libPath = path.resolve(__dirname, '../lib/package.json');
-      const data = JSON.stringify(minimalPackage, null, 2);
-      fse.writeFile(libPath, data, (err) => {
-        if (err) throw (err);
-        console.log(`Created package.json in ${libPath}`);
-        resolve();
+      return new Promise(resolve => {
+        const libPath = path.resolve(__dirname, "../lib/package.json");
+        const data = JSON.stringify(minimalPackage, null, 2);
+        fse.writeFile(libPath, data, err => {
+          if (err) throw err;
+          console.log(`Created package.json in ${libPath}`);
+          resolve();
+        });
       });
     });
-  });
 }
